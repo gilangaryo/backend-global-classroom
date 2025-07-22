ae20c5e12649f1cd9d6b51b4c9bac6847e3d4bfe
Gilang Aryo <gilang91668@gmail.com>
2025-07-23 01:16:16 +0700

    feat: update stripe route

--- src/routes/stripe.route.js
+++ src/routes/stripe.route.js
@@ -3,6 +3,8 @@ import { createCheckoutSession } from '../stripe/stripe.controller.js';
 import stripeWebhook from '../stripe/webhook.js';
 
 const router = express.Router();
-router.use('/', stripeWebhook);
+
+router.use('/webhook', stripeWebhook);
 router.post('/checkout', createCheckoutSession);
+
 export default router;
